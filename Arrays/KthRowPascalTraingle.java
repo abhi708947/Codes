835179a83c79ee0e0f58ebbddc8559422c80586e
ShreyaKarmakar <44618459+ShreyaKarmakar@users.noreply.github.com>
2021-08-27 20:19:33 +0530

    Update and rename KthRowPascalTraingle to KthRowPascalTraingle.java

--- Arrays/KthRowPascalTraingle.java
+++ Arrays/KthRowPascalTraingle.java
@@ -1,4 +1,4 @@
-Problem Description
+/*Problem Description
 
 Given an index k, return the kth row of the Pascal's triangle.
 Pascal's triangle: To generate A[C] in row R, sum up A'[C] and A'[C-1] from previous row R - 1.
@@ -12,7 +12,7 @@ Return : [1,3,3,1]
 
 
 approach : we have to return k th row after processing on k-1 th row, so have two arraylists one storing k-1 th and one storing kth.
-
+*/
 public class Solution {
     public ArrayList<Integer> getRow(int A) {
         ArrayList<Integer> add = new ArrayList<>();
